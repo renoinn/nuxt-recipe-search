cdc1c3e3ab6df259a70df538a31c39c98452e52b
renoinn <oomori.lunch@gmail.com>
2023-09-30 12:17:18 +0900

    set preset 'node-server'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true
+  },
+  nitro: {
+    preset: 'node-server',
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ],
