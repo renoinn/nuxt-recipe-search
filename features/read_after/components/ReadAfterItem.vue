6ba3765caec6ca405db739568788b42dba97bc30
renoinn <oomori.lunch@gmail.com>
2023-10-04 09:01:46 +0900

    feat: add read after list

--- features/read_after/components/ReadAfterItem.vue
+++ features/read_after/components/ReadAfterItem.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import { ResultItem } from '@/types/search'
+import { Recipe } from '@/types/recipes'
 
 const props = defineProps<{
-  item: ResultItem,
+  item: Recipe,
 }>()
 </script>
 
@@ -11,7 +11,7 @@ const props = defineProps<{
     <a :href="item.link" target="_blank">
       <h3>{{ item.title }}</h3>
       <p>{{ item.snippet }}</p>
-      <p><img :src="item.pagemap.cse_thumbnail[0].src"></p>
+      <p><img :src="item.thumbnail_src"></p>
       <p>{{ item.displayLink }}</p>
       <p><button>read after</button></p>
     </a>
---
+++ features/read_after/components/ReadAfterList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ReadAfterItem from '@/features/read_after/components/ReadAfterItem.vue'
+import { getRecipeList } from '@/features/read_after/composables/getRecipeList'
+
+const result = getRecipeList()
+</script>
+
+<template>
+  <ul v-if="result">
+    <li v-for="item in result.items" :key="item.link">
+      <ReadAfterItem :item="item" />
+    </li>
+  </ul>
+</template>
+
