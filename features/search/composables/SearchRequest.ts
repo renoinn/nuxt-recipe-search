ebd7fd85985544ab7d2175640260412868d81033
renoinn <oomori.lunch@gmail.com>
2023-10-06 09:46:17 +0900

    change return type to Recipe

--- features/search/composables/SearchRequest.ts
+++ features/search/composables/SearchRequest.ts
@@ -1,10 +1,11 @@
 import { SearchRequest, SearchResponse } from '@/types/search'
+import { Recipe } from '@/types/recipes'
 
 const domain = {
   api: ''
 }
 
-export const searchRequest = async (query: string): Promise<SearchResponse | null> => {
+export const searchRequest = async (query: string): Promise<Recipe[] | null> => {
   const params: SearchRequest = {
     q: query,
     start: 1,
@@ -16,6 +17,17 @@ export const searchRequest = async (query: string): Promise<SearchResponse | nul
     () => $fetch<SearchResponse>(`https://${domain.api}/search`, { params: params })
   )
 
-  return data.data.value
+  return data.data.value?.items.map((item) => {
+    const recipe: Recipe = {
+      title: item.title,
+      link: item.link,
+      displayLink: item.displayLink,
+      snippet: item.snippet,
+      thumbnail_src: item.pagemap.cse_thumbnail[0].src,
+      thumbnail_width: item.pagemap.cse_thumbnail[0].width,
+      thumbnail_height: item.pagemap.cse_thumbnail[0].height,
+    }
+    return recipe
+  }) || null
 }
 
