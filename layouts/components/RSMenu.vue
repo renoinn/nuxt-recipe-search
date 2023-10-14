49bcbf81a7cdea3e07e60157cbd907f4fb1be7f2
renoinn <oomori.lunch@gmail.com>
2023-10-14 11:42:18 +0900

    feat: add about page

--- layouts/components/RSMenu.vue
+++ layouts/components/RSMenu.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="mx-auto p-7">
     <ul class="text-center">
-      <li><NuxtLink to="/">Search</NuxtLink></li>
-      <li><NuxtLink to="/read_after">Read After</NuxtLink></li>
-    </ul>
-  </div>
-</template>
+      <li>
+        <NuxtLink to="/">Search</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/read_after">Read After</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+  </ul>
+</div></template>
 
---
+++ pages/about/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>About this site</h2>
+    <p>レシピ検索サービスです。</p>
+    <p>検索対象サイト</p>
+    <ul>
+      <li>Coockpad</li>
+      <li>etc.</li>
+    </ul>
+  </div>
+</template>
